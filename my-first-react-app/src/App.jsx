67d8e4db2394e5088df47a3d903f0eaa7dc73c26
Alekseenko Artem <alekseenkopoker@gmail.com>
2026-08-29 00:04:22 +0300

    Changed button on input

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,8 +3,14 @@ import './App.css'
 function App() {
 return (
   <div className='weather-block'>
-    <h1 className='weather-title'>Модуль погоды</h1>
-    <button className='weather-button'>Показать погоду</button>
+    <h1 className='weather-title'>Геокодер</h1>
+    <div className='container'>
+      <form className='iput-form'>
+        <label for='inp'>Укажите город:</label>
+        <input type='text' className='input-window' placeholder='Могилев' id='inp'></input>
+        <input type='submit'></input>
+      </form>
+    </div>
   </div>
 )
 }
