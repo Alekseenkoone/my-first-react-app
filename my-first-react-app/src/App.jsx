9abd17704db8905ad9cdd8aaac213a5b218ab227
Alekseenko Artem <alekseenkopoker@gmail.com>
2026-08-28 16:41:39 +0300

    started the project and added button and div for weather

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+return (
+  <div className='weather-block'>
+    <h1 className='weather-title'>Модуль погоды</h1>
+    <button className='weather-button'>Показать погоду</button>
+  </div>
+)
+}
+
+export default App
