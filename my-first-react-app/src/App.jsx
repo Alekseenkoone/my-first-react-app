34ee5ed7d40d76a247dcc58cd6e9aaf95788ea24
Alekseenko Artem <alekseenkopoker@gmail.com>
2026-08-29 16:30:32 +0300

    changed all logic and used jsonplaceholder as a server

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,53 @@
 import './App.css'
 
 function App() {
+
+let responseArr = []
+async function getUsers() {
+  try {
+    const response = 
+    await fetch('https://jsonplaceholder.typicode.com/users')
+    if (!response.ok) {
+      throw new Error(`Код ошибки ${response.status}`)
+    }
+    const result = await response.json()
+    console.log('my result: ', result)
+    document.getElementById('users-show-btn').classList.add('btn-user-show-Active')
+    responseArr = result
+    
+  }
+ catch(error) {
+  console.log("Не удалось получить данные", error)
+}
+}
+
+function showUsers() {
+  const usersWindow = document.querySelector('.users-window')
+  const usersList = document.createElement('ol')
+  usersWindow.append(usersList)
+  usersWindow.classList.remove('non-display')
+for (let i = 0; i < responseArr.length; i++) {
+  const listItem = document.createElement('li')
+  usersList.append(listItem)
+  listItem.textContent = responseArr[i].name
+}
+}
+
+
+
 return (
-  <div className='weather-block'>
-    <h1 className='weather-title'>Геокодер</h1>
-    <div className='container'>
-      <form className='iput-form'>
-        <label for='inp'>Укажите город:</label>
-        <input type='text' className='input-window' placeholder='Могилев' id='inp'></input>
-        <input type='submit'></input>
-      </form>
+  <div className='main'>
+  <div className='button-block'>
+    <h1 className='title'>Информация о пользователях</h1>
+    <div className='btn-container'>
+      <button className='btn btn-user-names' onClick={getUsers}>Запросить пользователей</button>
+      <button className='btn btn-user-show-noneActive' id='users-show-btn' onClick={showUsers}>Показать пользователей</button>
     </div>
   </div>
+    <div className='users-window non-display'>
+
+    </div>
+    </div>
 )
 }
 
